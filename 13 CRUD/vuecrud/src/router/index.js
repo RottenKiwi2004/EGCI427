4e385a6fde7e693561b496bfde8256300613834e
RottenKiwi <61570087+RottenKiwi2004@users.noreply.github.com>
2024-06-13 06:51:34 +0000

    Added 13 CRUD

---
+++ 13 CRUD/vuecrud/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Users from '../views/Users.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: Users
+    }
+  ]
+})
+
+export default router
